ba5dbd77a8d65f7458b064d1dcdae0c78f657f32
dbc2201 <divyanshb22@gmail.com>
2020-08-18 15:37:04 +0530

    add Video.java

---
+++ src/definitions/Video.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 18/08/20
+ *  Time: 3:36 PM
+ *  File Name : Video.java
+ * */
+package definitions;
+
+public class Video {
+}
