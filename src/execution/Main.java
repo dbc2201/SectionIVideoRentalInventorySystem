803cde820cf9feb83d68a298acaf399084849742
dbc2201 <divyanshb22@gmail.com>
2020-08-18 15:43:49 +0530

    add Main.java

---
+++ src/execution/Main.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 18/08/20
+ *  Time: 3:43 PM
+ *  File Name : Main.java
+ * */
+package execution;
+
+public class Main {
+}
